1bc69f7ce7a203b1bab02ac65aa307085048b3ff
Anshul <darkhunter1432004@gmail.com>
2024-12-22 18:38:59 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,63 @@
+let randomNumber=Math.floor(Math.random() * 10) + 1;
+let attempts=0;
+function checkGuess() 
+{
+    const guessInput=document.getElementById("guessInput").value;
+    const message=document.getElementById("message");
+    const guess=parseInt(guessInput,10);
+    attempts++;
+    if (isNaN(guess)) 
+    {
+        message.textContent="Please enter a valid number.";
+        return;
+    }
+    if (guess<1 || guess>10) 
+    {
+        message.textContent="Your guess must be between 1 and 10.";
+        return;
+    }
+
+    if (guess === randomNumber)
+    {
+        message.textContent=`Congratulations! You guessed the number ${randomNumber} in ${attempts} attempts.`;
+        message.style.color="green";
+        resetGame();
+    } 
+    else if (guess<randomNumber) 
+    {
+        if((randomNumber-guess)>=3)
+        {    
+            message.textContent = "Too Low!";
+            message.style.color = "red";
+        }
+        else
+        {
+            message.textContent = "A little Low!";
+            message.style.color = "red";
+        }
+    } 
+    else 
+    {
+        if((guess-randomNumber)>=3)
+        {    
+            message.textContent = "Too High!";
+            message.style.color = "red";
+        }
+        else
+        {
+            message.textContent = "A Little High!";
+            message.style.color = "red";
+        }
+    }
+}
+
+function resetGame() 
+{
+    setTimeout(()=> 
+    {
+        randomNumber=Math.floor(Math.random()*10)+1;
+        attempts = 0;
+        document.getElementById("guessInput").value = "";
+        document.getElementById("message").textContent = "";
+    }, 5000);
+}
